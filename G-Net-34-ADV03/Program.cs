89e37dba315ae8eaedefdaf9e816a8812658b4b6
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-03-26 17:12:50 +0200

    q1

--- G-Net-34-ADV03/Program.cs
+++ G-Net-34-ADV03/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1
+
+            List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+            Helper.PrintList("grades",grades);
+            int first = grades[0];
+            int lastgrade = grades[grades.Count - 1];
+            Console.WriteLine($"grade count :{grades.Count} ,first grade={first},last grade={lastgrade}");
+
+            grades.Sort();
+            Helper.PrintList("grades", grades);
+            int first90 = grades.Find(x => x > 90);
+            Console.WriteLine($" first grade above 90 :{first90} ");
+
+            List<int>below = grades.FindAll(x => x < 75);
+            Helper.PrintList("below", below);
+            grades.RemoveAll(x => x < 75);
+            Helper.PrintList("grades", grades);
+            bool any = grades.Exists(x => x == 100);
+            Console.WriteLine($"100 exists?{any}");
+             List<string> gradeStrings = grades.Select(g => "Grade: " + g).ToList();
+
+            foreach (var g in gradeStrings)
+            {
+                Console.WriteLine(g);
+            }
+            #endregion
         }
     }
 }
